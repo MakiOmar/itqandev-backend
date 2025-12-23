cd54b632f40b82229d25801472b9908d5d3f2f85
TaskFlow Developer <developer@taskflow.app>
2025-12-23 21:50:24 +0200

    fix(controllers): add missing Controller use statements

--- app/Http/Controllers/Api/BlogPostController.php
+++ app/Http/Controllers/Api/BlogPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\BlogPost;
 use App\Support\CacheKey;
 use Illuminate\Http\Request;
--- app/Http/Controllers/Api/TestimonialController.php
+++ app/Http/Controllers/Api/TestimonialController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
 use App\Models\Testimonial;
 use App\Support\CacheKey;
 use Illuminate\Http\Request;
