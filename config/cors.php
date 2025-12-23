fe2f607fa910efe4a1d93d26f455219ba2f27481
TaskFlow Developer <developer@taskflow.app>
2025-12-23 14:06:39 +0200

    fix: hardcode localhost cors origins

--- config/cors.php
+++ config/cors.php
@@ -14,12 +14,16 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:5173,http://127.0.0.1:3000,http://127.0.0.1:5173'))),
-
-    'allowed_origins_patterns' => [
-        '#^https?://localhost(:[0-9]+)?$#',
-        '#^https?://127\.0\.0\.1(:[0-9]+)?$#',
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:3000',
+        'http://127.0.0.1:5173',
+        'http://localhost',
+        'http://127.0.0.1',
     ],
+
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
