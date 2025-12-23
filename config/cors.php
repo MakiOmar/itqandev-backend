b3314f6de285c0389ac3a2e108cedc710de8f56d
TaskFlow Developer <developer@taskflow.app>
2025-12-23 13:37:54 +0200

    fix: broaden local cors patterns

--- config/cors.php
+++ config/cors.php
@@ -14,9 +14,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://localhost:5173,http://127.0.0.1:3000,http://127.0.0.1:5173')),
+    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', ''))),
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https?://localhost(:[0-9]+)?$#',
+        '#^https?://127\.0\.0\.1(:[0-9]+)?$#',
+    ],
 
     'allowed_headers' => ['*'],
 
